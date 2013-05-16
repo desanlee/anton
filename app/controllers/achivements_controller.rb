94647a4252372c335f57ee44d9b94077e20f2577
desanlee <desanlee@gmail.com>
2013-05-16 15:08:43 +0800

    add status

--- app/controllers/achivements_controller.rb
+++ app/controllers/achivements_controller.rb
@@ -2,11 +2,21 @@ class AchivementsController < ApplicationController
 
   def index
   
+	@currentweek = (Time.now).strftime("%W")
+	@currentweekbegin = (Time.now).at_beginning_of_week.strftime("%m.%d")
+	@currentweekend = ((Time.now).at_beginning_of_week + 4.day).strftime("%m.%d")
 	if current_user.engineer? then
-		@mysysconfigurations = current_user.sysconfigs
-		@myexecutions = current_user.executions
-		@mytestcases = current_user.testcases
-		@mybugs = @myexecutions.find_all_by_result('fail') if @myexecutions != nil
+		@mysysconfigurations = Array.new
+		current_user.sysconfigs.each do |c|
+			@mysysconfigurations << c if c.created_at.strftime("%W") == @currentweek
+		end
+		
+		@myexecutions = Array.new
+		current_user.executions.each do |e|
+			@myexecutions << e if e.created_at.strftime("%W") == @currentweek
+		end
+		
+		
 	end
 	
   end
