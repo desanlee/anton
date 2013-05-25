df29540496b3669d5692461a5d66ad352c8a16a0
desanlee <desanlee@gmail.com>
2013-05-25 19:50:05 +0800

    bug fix

--- app/models/realconfig.rb
+++
@@ -1,7 +0,0 @@
-class Realconfig < ActiveRecord::Base
-  attr_accessible :device_id, :devicename, :devicetype, :targetmatrix_id
-  
-  belongs_to :device, class_name: "Device"
-  belongs_to :targetmatrix, class_name: "Targetmatrix"
-  
-end
