cc0c63c730181c6f6aa7a7b79513ae4ab50da54f
desanlee <desanlee@gmail.com>
2013-05-30 09:52:00 +0800

    bug fix

--- app/controllers/targetdeprelationships_controller.rb
+++ app/controllers/targetdeprelationships_controller.rb
@@ -3,7 +3,7 @@ class TargetdeprelationshipsController < ApplicationController
   def destroy
     @targetdeprelationship = Targetdeprelationship.find(params[:id])
 	@targetdeprelationship.destroy
-	redirect_to :tasks
+	redirect_to :tasks, :action => "index", :selecttask => params[:selecttask], :selecttarget => params[:selecttarget]
   end
   
 end
