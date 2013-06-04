476328a5bb64c9b7a75f55b9e0cb2a9dfd19ffe4
desanlee <desanlee@gmail.com>
2013-06-04 14:45:54 +0800

    add edit sut

--- app/controllers/suts_controller.rb
+++ app/controllers/suts_controller.rb
@@ -1,5 +1,19 @@
 class SutsController < ApplicationController
 
+  def edit
+    @sut = Sut.find(params[:id])
+	render :layout => "justapage"
+  end
+  
+  def update
+    @sut = Sut.find(params[:id])
+    if @sut.update_attributes(params[:sut])
+       render :text => '<script type="text/javascript"> window.close() </script>'
+    else
+      render 'edit'
+    end
+  end
+  
   def destroy
     @sut = Sut.find(params[:id])
 	@sut.destroy
